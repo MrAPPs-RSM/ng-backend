eb4609f510f694b1159cbff895ff7ade9b2c7e6a
damiandominella <damiandominella@gmail.com>
2017-05-24 12:08:49 +0200

    fixing and multiple choice and deletion

--- src/app/pages/form/components/calendar/calendar.component.ts
+++ src/app/pages/form/components/calendar/calendar.component.ts
@@ -1,40 +1,52 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { CalendarEvent } from 'angular-calendar';
-import { setHours, setMinutes, isSameDay, isSameMonth } from 'date-fns';
+import { setHours, setMinutes, isSameDay } from 'date-fns';
 import { Subject } from 'rxjs';
 import { ApiService } from '../../../../api';
+import { isNullOrUndefined } from 'util';
 
 @Component({
     selector: 'calendar',
-    styleUrls: ['calendar.scss'],
+    styleUrls: ['./calendar.scss'],
     templateUrl: './calendar.html'
 })
 export class Calendar implements OnInit {
 
+    /** Fixed values */
     EVENT_ACTIONS: any[] = [{
         label: '<i class="fa fa-fw fa-times"></i>',
         onClick: ({event}: { event: CalendarEvent }): void => {
             this.events = this.events.filter(iEvent => iEvent !== event);
             this.activeDayIsOpen = false;
+            this.refreshFormValue();
         }
     }];
     MAX_EVENTS_SHOWN: number = 4;
     EVENT_ICON: string = '';
-    COLORS: any[] = [];
+    COLORS: any = {
+        default: {
+            primary: '#000000',
+            secondary: '#ffffff'
+        },
+        custom: []
+    };
 
+    /** I/O */
     @Input() form: FormGroup;
     @Input() field: any = {};
+    @Output() eventClicked: EventEmitter<{ event: CalendarEvent }> = new EventEmitter<{ event: CalendarEvent }>();
 
+    /** Calendar values */
     view: string = 'month';
     viewDate: Date;
     refresh: Subject<any> = new Subject();
     events: any[] = [];
     activeDayIsOpen: boolean = false;
-    @Output() eventClicked: EventEmitter<{ event: CalendarEvent }> = new EventEmitter<{ event: CalendarEvent }>();
 
+    /** Select and event creation values */
     showSelect: boolean = false;
-    selectedValue: any = null;
+    selectedValues: any = null;
     selectedDays: any[] = [];
     selectValues: any[] = [];
 
@@ -42,20 +54,25 @@ export class Calendar implements OnInit {
     }
 
     ngOnInit() {
+        if (this.field.options.multipleSelect) {
+            this.selectedValues = [];
+        }
         this.viewDate = new Date();
         this.EVENT_ICON = this.field.options.eventIcon;
-        this.COLORS = this.field.options.eventColors;
+        this.COLORS.custom = this.field.options.eventColors;
         this.loadSelectValues();
     }
 
     loadSelectValues(): void {
         if (this.field.options.selectOptions instanceof Array) {
-            this.transformSelectValues(this.field.options.selectOptions);
+            this.selectValues = this.field.options.selectOptions;
+            // this.transformSelectValues(this.field.options.selectOptions);
         } else {
             this._apiService.get(this.field.options.selectOptions, false)
                 .subscribe(
                     data => {
-                        this.transformSelectValues(data);
+                        this.selectValues = data;
+                        // this.transformSelectValues(data);
                     },
                     error => {
                         console.log(error);
@@ -65,15 +82,16 @@ export class Calendar implements OnInit {
         }
     }
 
-    transformSelectValues(array: [{ id: any, text: string }]): void {
+    /* transformSelectValues(array: [{ id: any, text: string }]): void {
         array.forEach((item) => {
+            let color = !isNullOrUndefined(this.COLORS.custom[item.id]) ?
+                this.COLORS.custom[item.id].primary : this.COLORS.default;
             this.selectValues.push({
                 id: item.id,
-                text: '<i class="' + this.EVENT_ICON + '" style="color:'
-                + this.COLORS[item.id].primary + '"></i>&nbsp;' + item.text
+                text: '<i class="' + this.EVENT_ICON + '" style="color:' + color + '"></i>&nbsp;' + item.text
             });
         });
-    }
+    } */
 
     onEventClick(mouseEvent: MouseEvent, calendarEvent: CalendarEvent): void {
         if (mouseEvent.stopPropagation) {
@@ -103,24 +121,76 @@ export class Calendar implements OnInit {
     }
 
     confirmCreation(): void {
-        console.log(this.selectedValue);
+
         this.selectedDays.forEach((day) => {
-            this.events.push({
-                title: this.selectedValue.text,
-                start: day.date,
-                color: this.COLORS[this.selectedValue.id],
-                icon: this.EVENT_ICON,
-                actions: this.EVENT_ACTIONS
-            });
+            if (this.selectedValues instanceof Array) {
+                this.selectedValues.forEach((selectedValue) => {
+                    this.events.push({
+                        id: selectedValue.id,
+                        title: selectedValue.text,
+                        start: setHours(setMinutes(day.date, 0), 0),
+                        color: !isNullOrUndefined(this.COLORS.custom[selectedValue.id]) ?
+                            this.COLORS.custom[selectedValue.id] : this.COLORS.default,
+                        icon: this.EVENT_ICON,
+                        actions: this.EVENT_ACTIONS
+                    });
+                });
+            } else {
+                this.events.push({
+                    id: this.selectedValues.id,
+                    title: this.selectedValues.text,
+                    start: setHours(setMinutes(day.date, 0), 0),
+                    color: !isNullOrUndefined(this.COLORS.custom[this.selectedValues.id]) ?
+                        this.COLORS.custom[this.selectedValues.id] : this.COLORS.default,
+                    icon: this.EVENT_ICON,
+                    actions: this.EVENT_ACTIONS
+                });
+            }
         });
 
         this.selectedDays = [];
         this.refresh.next();
         this.dismissCreation();
+        this.refreshFormValue();
     }
 
     dismissCreation(): void {
         this.showSelect = false;
-        this.selectedValue = null;
+        this.selectedValues = [];
+    }
+
+    refreshFormValue(): void {
+        let data = [];
+        this.events.forEach((event) => {
+            data.push({
+                eventId: event.id,
+                date: event.start
+            });
+        });
+        this.form.controls[this.field.key].setValue(data);
+    }
+
+    clearAllEventsByDay(): void {
+        let indexes: number[] = [];
+        this.selectedDays.forEach((day) => {
+            this.events.forEach((event, index) => {
+                if (event.start.getDate() === day.date.getDate()
+                && event.start.getMonth() === day.date.getMonth()
+                && event.start.getUTCFullYear() === day.date.getUTCFullYear()) {
+                    indexes.push(index);
+                }
+            });
+        });
+
+        this.selectedDays = [];
+        console.log(indexes);
+        if (indexes.length > 0) {
+            indexes.forEach((index) => {
+                console.log('removing ' + this.events[index]);
+                delete this.events[index];
+            });
+            this.refresh.next();
+            this.refreshFormValue();
+        }
     }
 }
