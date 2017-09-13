2ab0db5eb6ef590ad7c4e134e3bdc02542b33dbe
damiandominella <damiandominella@gmail.com>
2017-09-13 12:20:10 +0200

    fix

--- src/app/pages/form/components/latlng/lat-lng.component.ts
+++ src/app/pages/form/components/latlng/lat-lng.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { isNullOrUndefined } from 'util';
-import {ln} from "shelljs";
 
 @Component({
     selector: 'lat-lng',
